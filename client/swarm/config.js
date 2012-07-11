65af5fe0f16726b86d370fa6c90ff0848fdc0a2c
Andrew Tergis <andrew.tergi@buglabs.net>
2012-07-11 19:22:58 +0000

    Add config structure

---
+++ client/swarm/config.js
@@ -0,0 +1,11 @@
+var config = {}
+
+config.pushHostname = '';
+
+config.apikey = '';
+config.loopbackResource = '';
+config.loopbackSwarm = '';
+config.loopbackToken = 'loopback';
+config.pollInterval = 30000;
+config.swarmTimeout = 3000;
+
