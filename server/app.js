59b81c654c820ed69e9c31268088925941def9a2
Andrew Tergis <andrew.tergi@buglabs.net>
2012-07-19 13:34:53 +0000

    Updated MVC with new mongo data, added basic OK coloring

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
 var webservice = require('./webservice');
-var service = new Webservice(8080, this);
+var service = new Webservice(8080);
 var io = require('socket.io').listen(8081);
 
-//rudimentary k/v store in memory:
-exports.history = {};
-
 service.on('serverdata', function(data) {
-	console.log(JSON.stringify(data));
-	exports.history[data.host] = data;
-	console.log(exports.history);
+	var host = data.host;
+	console.log('Got event about host; '+host);
+	service.history[host].online = true;
+	if ('timeout' in service.history[host]) {
+		clearTimeout(service.history[host].timeout);
+	}
+	//service.history[host].timeout = setTimeout(function() {
+	//	service.history[host].online = false;
+	//	console.log('Host '+host+' has fallen offline!');
+	//}, service.history[host].rate*3);
+	io.sockets.emit('update', service.history[host]);
 });
 
-service.on('requestdata', function(host) {
-
+io.sockets.on('connection', function(socket) {
+	console.log('new socket: '+socket.id);
+	socket.emit('history', service.history);
 });
--- server/webservice.js
+++ server/webservice.js
@@ -1,44 +1,47 @@
 var events = require('events');
 var util = require('util');
 var express = require('express');
-var app = express.createServer()
 var eventEmitter = new events.EventEmitter();
 
 //simple k/v last value store
-exports.history = {};
 
 Webservice = function(port) {
+	var self = this;
+	var history = self.history = {};
+	self.name = "Hi";
+	self.app = express.createServer()
 	events.EventEmitter.call(this);
-	app.listen(port);
-	console.log('[webservice] listening on port '+app.address().port);
+	self.app.listen(port);
+	console.log('[webservice] listening on port '+self.app.address().port);
+
+	self.app.use(express.bodyParser());
+
+	self.app.post('/set', function(req, res) {
+		if ('host' in req.body) {
+			console.log(req.body);
+			res.send("OK");
+			history[req.body.host] = req.body;
+			console.log('emitting event');
+			self.emit('serverdata', req.body);
+		} else {
+			res.send("Host not specified",400);
+		}
+	});
+
+	self.app.get('/get', function(req, res) {
+		res.send(JSON.stringify(history));
+	});
+
+	self.app.get('/get/:host', function(req, res) {
+		var host = req.params.host;
+		if (host in history) {
+			res.send(JSON.stringify(history[req.params.host]));
+		} else {
+			res.send('{}', 404);
+		}
+	});
+
+	self.app.use(express.static(__dirname+'/static'));
 }
 util.inherits(Webservice, events.EventEmitter);
-
-app.use(express.bodyParser());
-
-app.post('/set', function(req, res) {
-	if ('host' in req.body) {
-		console.log(req.body);
-		res.send("OK");
-		exports.history[req.body.host] = req.body;
-		eventEmitter.emit('serverdata', req.body);
-	} else {
-		res.send("Host not specified",400);
-	}
-});
-
-app.get('/get', function(req, res) {
-	res.send(JSON.stringify(exports.history));
-});
-
-app.get('/get/:host', function(req, res) {
-	var host = req.params.host;
-	if (host in exports.history) {
-		res.send(JSON.stringify(exports.history[req.params.host]));
-	} else {
-		res.send('{}', 404);
-	}
-});
-
-app.use(express.static(__dirname+'/static'));
 
